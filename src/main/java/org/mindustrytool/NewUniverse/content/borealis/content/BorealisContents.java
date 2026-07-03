93c5eff3a0524b1677c9edf4e11794eb7e9bd88d
neko-chan <meohexa.1.a@gmail.com>
2026-07-03 15:44:47 +0700

    feat: register distribution, power, and production blocks in BorealisContents

--- src/main/java/org/mindustrytool/NewUniverse/content/borealis/content/BorealisContents.java
+++ src/main/java/org/mindustrytool/NewUniverse/content/borealis/content/BorealisContents.java
@@ -4,6 +4,9 @@ import mindustry.type.Item;
 import mindustry.type.UnitType;
 import mindustry.world.Block;
 import org.mindustrytool.NewUniverse.content.borealis.content.building.BorealisBuildingBlocks;
+import org.mindustrytool.NewUniverse.content.borealis.content.building.BorealisDistributionBlocks;
+import org.mindustrytool.NewUniverse.content.borealis.content.building.BorealisPowerBlocks;
+import org.mindustrytool.NewUniverse.content.borealis.content.building.BorealisProductionBlocks;
 import org.mindustrytool.NewUniverse.content.borealis.content.building.BorealisTurretBlocks;
 import org.mindustrytool.NewUniverse.content.borealis.content.enviroment.BorealisEnvironmentBlocks;
 import org.mindustrytool.NewUniverse.content.borealis.content.enviroment.ore.BorealisOreBlocks;
@@ -32,6 +35,9 @@ public class BorealisContents {
     private final BorealisJuniorUnitTypes junior;
     private final BorealisDiphdaUnitTypes air;
     private final BorealisBuildingBlocks buildingBlocks;
+    private final BorealisDistributionBlocks distributionBlocks;
+    private final BorealisPowerBlocks powerBlocks;
+    private final BorealisProductionBlocks productionBlocks;
     private final BorealisTurretBlocks turretBlocks;
     private final BorealisEnvironmentBlocks environmentBlocks;
     private final BorealisOreBlocks oreBlocks;
@@ -97,6 +103,46 @@ public class BorealisContents {
     public Block coreBasis;
     public Block coreCentrum;
     public Block corePreatorium;
+
+    // distribution
+    public Block conduit;
+    public Block conduitRouter;
+    public Block conduitJunction;
+    public Block conduitBridge;
+    public Block liquidContainer;
+    public Block liquidTank;
+    public Block duct;
+    public Block armoredDuct;
+    public Block ductRouter;
+    public Block ductBridge;
+    public Block overflowDuct;
+    public Block unoverflowDuct;
+
+    // power (preview)
+    public Block pwr2a;
+    public Block pwr2b;
+    public Block pwr3a;
+    public Block pwr3b;
+    public Block pwr3c;
+    public Block pwr3d;
+    public Block pwr4a;
+
+    // production (preview)
+    public Block prod2a;
+    public Block prod2b;
+    public Block prod2c;
+    public Block prod2d;
+    public Block prod2e;
+    public Block prod2f;
+    public Block prod2g;
+    public Block prod2h;
+    public Block prod2i;
+    public Block prod2k;
+    public Block prod3a;
+    public Block prod3b;
+    public Block prod3c;
+    public Block prod3d;
+    public Block prod3e;
 
     // turrets
     public Block ferios;
@@ -171,6 +217,46 @@ public class BorealisContents {
         coreBasis = buildingBlocks.coreBasis;
         coreCentrum = buildingBlocks.coreCentrum;
         corePreatorium = buildingBlocks.corePreatorium;
+
+        distributionBlocks.loadContent();
+        conduit = distributionBlocks.conduit;
+        conduitRouter = distributionBlocks.conduitRouter;
+        conduitJunction = distributionBlocks.conduitJunction;
+        conduitBridge = distributionBlocks.conduitBridge;
+        liquidContainer = distributionBlocks.liquidContainer;
+        liquidTank = distributionBlocks.liquidTank;
+        duct = distributionBlocks.duct;
+        armoredDuct = distributionBlocks.armoredDuct;
+        ductRouter = distributionBlocks.ductRouter;
+        ductBridge = distributionBlocks.ductBridge;
+        overflowDuct = distributionBlocks.overflowDuct;
+        unoverflowDuct = distributionBlocks.unoverflowDuct;
+
+        powerBlocks.loadContent();
+        pwr2a = powerBlocks.pwr2a;
+        pwr2b = powerBlocks.pwr2b;
+        pwr3a = powerBlocks.pwr3a;
+        pwr3b = powerBlocks.pwr3b;
+        pwr3c = powerBlocks.pwr3c;
+        pwr3d = powerBlocks.pwr3d;
+        pwr4a = powerBlocks.pwr4a;
+
+        productionBlocks.loadContent();
+        prod2a = productionBlocks.prod2a;
+        prod2b = productionBlocks.prod2b;
+        prod2c = productionBlocks.prod2c;
+        prod2d = productionBlocks.prod2d;
+        prod2e = productionBlocks.prod2e;
+        prod2f = productionBlocks.prod2f;
+        prod2g = productionBlocks.prod2g;
+        prod2h = productionBlocks.prod2h;
+        prod2i = productionBlocks.prod2i;
+        prod2k = productionBlocks.prod2k;
+        prod3a = productionBlocks.prod3a;
+        prod3b = productionBlocks.prod3b;
+        prod3c = productionBlocks.prod3c;
+        prod3d = productionBlocks.prod3d;
+        prod3e = productionBlocks.prod3e;
 
         turretBlocks.loadContent();
         ferios = turretBlocks.ferios;
