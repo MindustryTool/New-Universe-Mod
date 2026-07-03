b53455f82058b9431a4228fd75ce72b341dd1ee8
neko-chan <meohexa.1.a@gmail.com>
2026-07-03 17:31:31 +0700

    feat: add environment prop blocks (boulders, crystals)

--- src/main/java/org/mindustrytool/NewUniverse/content/borealis/content/enviroment/BorealisEnvironmentBlocks.java
+++ src/main/java/org/mindustrytool/NewUniverse/content/borealis/content/enviroment/BorealisEnvironmentBlocks.java
@@ -1,15 +1,37 @@
 package org.mindustrytool.NewUniverse.content.borealis.content.enviroment;
 
-import lombok.RequiredArgsConstructor;
+import mindustry.world.Block;
+import mindustry.world.blocks.environment.Prop;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import lombok.RequiredArgsConstructor;
 
 @Singleton
 @RequiredArgsConstructor(onConstructor_ = @Inject)
 public class BorealisEnvironmentBlocks {
 
-    public void loadContent() {
+    // props (decorative, no collision)
+    public Block boulder;
+    public Block redBoulder;
+    public Block iceBoulder;
+    public Block darkblueBoulder;
+    public Block darkDirtBoulder;
+    public Block redCrystalCluster;
+    public Block blueCrystalBlocks;
 
+    public void loadContent() {
+        props();
+    }
+
+    private void props() {
+        boulder = new Prop("boulder") {{ variants = 3; }};
+        redBoulder = new Prop("red-boulder") {{ variants = 3; }};
+        iceBoulder = new Prop("ice-boulder") {{ variants = 3; }};
+        darkblueBoulder = new Prop("darkblue-boulder") {{ variants = 3; }};
+        darkDirtBoulder = new Prop("dark-dirt-boulder") {{ variants = 3; }};
+        redCrystalCluster = new Prop("red-crystal-cluster") {{ variants = 3; }};
+        blueCrystalBlocks = new Prop("blue-crystal-blocks") {{ variants = 3; }};
     }
 }
