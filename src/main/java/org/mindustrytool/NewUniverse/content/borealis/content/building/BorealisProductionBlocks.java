646639a6978768d1489fa932d68cb56f96255cfd
neko-chan <meohexa.1.a@gmail.com>
2026-07-04 12:51:32 +0700

    refactor: update ProductionBlocks with new crafting recipes

--- src/main/java/org/mindustrytool/NewUniverse/content/borealis/content/building/BorealisProductionBlocks.java
+++ src/main/java/org/mindustrytool/NewUniverse/content/borealis/content/building/BorealisProductionBlocks.java
@@ -1,7 +1,13 @@
 package org.mindustrytool.NewUniverse.content.borealis.content.building;
 
+import mindustry.content.Fx;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.production.GenericCrafter;
+
+import org.mindustrytool.NewUniverse.content.borealis.content.item.BorealisItems;
+import org.mindustrytool.NewUniverse.content.borealis.content.item.BorealisLiquids;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -11,82 +17,137 @@ import lombok.RequiredArgsConstructor;
 @Singleton
 @RequiredArgsConstructor(onConstructor_ = @Inject)
 public class BorealisProductionBlocks {
+    private final BorealisItems items;
+    private final BorealisLiquids liquids;
 
-    public Block prod2a;
-    public Block prod2b;
-    public Block prod2c;
-    public Block prod2d;
-    public Block prod2e;
-    public Block prod2f;
-    public Block prod2g;
-    public Block prod2h;
-    public Block prod2i;
-    public Block prod2k;
-    public Block prod3a;
-    public Block prod3b;
-    public Block prod3c;
-    public Block prod3d;
-    public Block prod3e;
+    // TIER 1 - MIDGARD
+
+    public Block runeForge;
+
+    public Block alfKiln;
+    public Block flaxolCrucible;
+
+    public Block dwarfAnvil;
+    public Block farasSmelter;
+
+    public Block seidrCauldron;
+    public Block aetherForge;
+
+    public Block aesirResonator;
+    public Block odinsForge;
 
     public void loadContent() {
-        prod2a = new GenericCrafter("prod-2a") {{
+        runeForge = new GenericCrafter("rune-forge") {{
+            requirements(Category.crafting, ItemStack.with(items.rudis, 30, items.pausis, 10));
             size = 2;
+            craftTime = 60f;
+            hasPower = true;
+            hasItems = true;
+            consumePower(1f);
+            consumeItems(ItemStack.with(items.rudis, 3, items.pausis, 1));
+            outputItem = new ItemStack(items.simus, 2);
+            craftEffect = Fx.none;
+            updateEffect = Fx.none;
         }};
 
-        prod2b = new GenericCrafter("prod-2b") {{
+        alfKiln = new GenericCrafter("alf-kiln") {{
+            requirements(Category.crafting, ItemStack.with(items.sand, 30));
             size = 2;
+            craftTime = 45f;
+            hasPower = true;
+            hasItems = true;
+            consumePower(1.5f);
+            consumeItems(ItemStack.with(items.sand, 3));
+            outputItem = new ItemStack(items.temperedGlass, 2);
+            craftEffect = Fx.none;
         }};
 
-        prod2c = new GenericCrafter("prod-2c") {{
+        flaxolCrucible = new GenericCrafter("flaxol-crucible") {{
+            requirements(Category.crafting, ItemStack.with(items.sand, 30, items.pausis, 15));
             size = 2;
+            craftTime = 75f;
+            hasPower = true;
+            hasItems = true;
+            consumePower(1.2f);
+            consumeItems(ItemStack.with(items.sand, 3, items.pausis, 1));
+            outputItem = new ItemStack(items.flaxol, 2);
+            craftEffect = Fx.none;
         }};
 
-        prod2d = new GenericCrafter("prod-2d") {{
+        dwarfAnvil = new GenericCrafter("dwarf-anvil") {{
+            requirements(Category.crafting, ItemStack.with(items.simus, 30, items.temperedGlass, 20));
             size = 2;
+            craftTime = 90f;
+            hasPower = true;
+            hasItems = true;
+            consumePower(2f);
+            consumeItems(ItemStack.with(items.simus, 2, items.temperedGlass, 1));
+            outputItem = new ItemStack(items.fabris, 2);
+            craftEffect = Fx.none;
         }};
 
-        prod2e = new GenericCrafter("prod-2e") {{
+        farasSmelter = new GenericCrafter("faras-smelter") {{
+            requirements(Category.crafting, ItemStack.with(items.simus, 20, items.flaxol, 20));
             size = 2;
+            craftTime = 80f;
+            hasPower = true;
+            hasItems = true;
+            consumePower(2.5f);
+            consumeItems(ItemStack.with(items.simus, 1, items.flaxol, 1));
+            outputItem = new ItemStack(items.farasAlloy, 2);
+            craftEffect = Fx.none;
         }};
 
-        prod2f = new GenericCrafter("prod-2f") {{
-            size = 2;
-        }};
-
-        prod2g = new GenericCrafter("prod-2g") {{
-            size = 2;
-        }};
-
-        prod2h = new GenericCrafter("prod-2h") {{
-            size = 2;
-        }};
-
-        prod2i = new GenericCrafter("prod-2i") {{
-            size = 2;
-        }};
-
-        prod2k = new GenericCrafter("prod-2k") {{
-            size = 2;
-        }};
-
-        prod3a = new GenericCrafter("prod-3a") {{
+        seidrCauldron = new GenericCrafter("seidr-cauldron") {{
+            requirements(Category.crafting, ItemStack.with(items.fabris, 30, items.vastum, 20));
             size = 3;
+            craftTime = 120f;
+            hasPower = true;
+            hasItems = true;
+            hasLiquids = true;
+            consumePower(3f);
+            consumeItems(ItemStack.with(items.fabris, 2, items.vastum, 1));
+            consumeLiquid(liquids.barbavior, 0.3f);
+            outputItem = new ItemStack(items.tentias, 1);
+            craftEffect = Fx.none;
         }};
 
-        prod3b = new GenericCrafter("prod-3b") {{
+        aetherForge = new GenericCrafter("aether-forge") {{
+            requirements(Category.crafting, ItemStack.with(items.farasAlloy, 40, items.fabris, 20));
             size = 3;
+            craftTime = 110f;
+            hasPower = true;
+            hasItems = true;
+            hasLiquids = true;
+            consumePower(3.5f);
+            consumeItems(ItemStack.with(items.farasAlloy, 2, items.fabris, 1));
+            consumeLiquid(liquids.horani, 0.2f);
+            outputItem = new ItemStack(items.navitas, 2);
+            craftEffect = Fx.none;
         }};
 
-        prod3c = new GenericCrafter("prod-3c") {{
+        aesirResonator = new GenericCrafter("aesir-resonator") {{
+            requirements(Category.crafting, ItemStack.with(items.navitas, 50, items.tentias, 30, items.farasAlloy, 30));
             size = 3;
+            craftTime = 150f;
+            hasPower = true;
+            hasItems = true;
+            consumePower(5f);
+            consumeItems(ItemStack.with(items.navitas, 2, items.tentias, 1, items.farasAlloy, 1));
+            outputItem = new ItemStack(items.duras, 2);
+            craftEffect = Fx.none;
         }};
 
-        prod3d = new GenericCrafter("prod-3d") {{
-            size = 3;
-        }};
-
-        prod3e = new GenericCrafter("prod-3e") {{
-            size = 3;
+        odinsForge = new GenericCrafter("odins-forge") {{
+            requirements(Category.crafting, ItemStack.with(items.duras, 60, items.navitas, 60, items.tentias, 40));
+            size = 4;
+            craftTime = 240f;
+            hasPower = true;
+            hasItems = true;
+            consumePower(10f);
+            consumeItems(ItemStack.with(items.duras, 2, items.navitas, 2, items.tentias, 1));
+            outputItem = new ItemStack(items.cophalast, 2);
+            craftEffect = Fx.none;
         }};
     }
 }
